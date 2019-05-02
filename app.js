3660abe4424d2efbbba477fb9ee4cba9d2e97dd1
Julio Bueso <julio.ezequiel@gmail.com>
2019-05-02 16:32:12 -0600

    Orden

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express')
 var app = express()
 var fs = require('fs');
 const readline = require("readline");
-var resultadosOrden = [];
+var resultadosFinalProcesado = [];
 
 app.get('/', function(req, res) {
-
+  var resultadosOrden = [];
 
   // fs.readFile('archivo.txt', 'utf-8', (err, data) => {
   //   if (err) {
@@ -22,11 +22,52 @@ app.get('/', function(req, res) {
     input: fs.createReadStream(NOMBRE_ARCHIVO)
   });
 
+
   lector.on("line", linea => {
-    resultadosOrden.push(linea)
+    var devuelveResultados = '';
+    var contador = 0;
+    var contadorPar = 0;
+    var contadorImpar = 0;
+    var arrayResultado = linea.split("-").map(String);
+    var ordenArray = [];
+
+    function comparar(a, b) {
+      return a - b;
+    }
+
+    arrayResultado.sort(comparar);
+
+    arrayResultado.forEach(item => {
+
+      contador++;
+      if (contador == 1) {
+        devuelveResultados = devuelveResultados + item;
+      } else {
+        devuelveResultados = devuelveResultados + '-' + item;
+      }
+
+      if (item % 2) {
+        contadorImpar++;
+      } else {
+        contadorPar++;
+      }
+
+    })
+    // console.log(devuelveResultados);
+    if (contadorPar > contadorImpar) {
+      console.log("Par" + contadorPar);
+    } else {
+      console.log("Impar" + contadorImpar);
+    }
+
+    if (contadorPar == contadorImpar) {
+      console.log("igual");
+    }
+
+
   });
-  console.log(resultadosOrden);
-  res.send(resultadosOrden)
+
+  res.send("Terminado")
 })
 
 app.listen(3000)
