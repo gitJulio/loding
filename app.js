9fbc7618d3387470853f1c5285c16b6660b35f1e
gitjulio <julio.ezequiel@gmail.com>
2019-05-09 22:03:40 -0600

    Hola

--- app.js
+++ app.js
@@ -7,122 +7,122 @@ var bodyParser = require('body-parser')
 
 app.get('/operacion/:opcion', function(req, res) {
 
-  var resultadosOrden;;
+    var resultadosOrden;;
 
-  // fs.readFile('archivo.txt', 'utf-8', (err, data) => {
-  //   if (err) {
-  //     console.log('error: ', err);
-  //   } else {
-  //     res.send(data)
-  //   }
-  // });
-
-
-  NOMBRE_ARCHIVO = "archivo.txt";
-
-  let lector = readline.createInterface({
-    input: fs.createReadStream(NOMBRE_ARCHIVO)
-  });
+    // fs.readFile('archivo.txt', 'utf-8', (err, data) => {
+    //   if (err) {
+    //     console.log('error: ', err);
+    //   } else {
+    //     res.send(data)
+    //   }
+    // });
 
 
-  lector.on("line", linea => {
+    NOMBRE_ARCHIVO = "archivo.txt";
 
-    resultadosOrden = 'a';
-    var devuelveResultados = '';
-    var contador = 0;
-    var contadorPar = 0;
-    var contadorImpar = 0;
-    var arrayResultado = linea.split("-").map(String);
-    var ordenArray = [];
+    let lector = readline.createInterface({
+        input: fs.createReadStream(NOMBRE_ARCHIVO)
+    });
 
-    var secuencia1 = 0;
-    var secuencia2 = 0;
-    var secuencia3 = 0;
-    var secuencia4 = 0;
 
-    function comparar(a, b) {
-      return a - b;
+    lector.on("line", linea => {
+
+        resultadosOrden = 'a';
+        var devuelveResultados = '';
+        var contador = 0;
+        var contadorPar = 0;
+        var contadorImpar = 0;
+        var arrayResultado = linea.split("-").map(String);
+        var ordenArray = [];
+
+        var secuencia1 = 0;
+        var secuencia2 = 0;
+        var secuencia3 = 0;
+        var secuencia4 = 0;
+
+        function comparar(a, b) {
+            return a - b;
+        }
+
+        arrayResultado.sort(comparar);
+
+        arrayResultado.forEach(item => {
+
+            contador++;
+            if (contador == 1) {
+                devuelveResultados = devuelveResultados + item;
+            } else {
+                devuelveResultados = devuelveResultados + '-' + item;
+            }
+
+            if (item % 2 == 1) {
+                contadorImpar++;
+            } else {
+                contadorPar++;
+            }
+
+            if (item < 9) {
+                secuencia1++;
+            }
+
+            if (item > 9 && item < 20) {
+                secuencia2++;
+            }
+
+            if (item > 19 && item < 30) {
+                secuencia3++;
+            }
+
+            if (item > 29) {
+                secuencia4++;
+            }
+
+        })
+        if (req.params.opcion == 1) {
+            // console.log(devuelveResultados);
+            asigna(devuelveResultados)
+
+        }
+        /******************TIPO**********/
+        if (req.params.opcion == 2) {
+            if (contadorPar > contadorImpar) {
+                // console.log("Par" + contadorPar);
+                asigna("Par" + contadorPar)
+            }
+
+            if (contadorPar < contadorImpar) {
+                // console.log("Impar" + contadorImpar);
+                asigna("Impar" + contadorImpar)
+            }
+
+            if (contadorPar == contadorImpar) {
+                // console.log("igual");
+                asigna("igual")
+            }
+        }
+        /***************FIN TIPO********/
+
+        /*************SECUENCIA********/
+
+        if (req.params.opcion == 3) {
+            // console.log(secuencia1 + " - " + secuencia2 + " - " + secuencia3 + " - " + secuencia4);
+            asigna(secuencia1 + " - " + secuencia2 + " - " + secuencia3 + " - " + secuencia4)
+        }
+
+        /**********FIN SECUENCIA*******/
+        contadorImpar, contadorImpra = 0;
+        console.log("");
+    });
+
+    var ver = [];
+    async function asigna(data) {
+        ver.push(data)
     }
 
-    arrayResultado.sort(comparar);
 
-    arrayResultado.forEach(item => {
-
-      contador++;
-      if (contador == 1) {
-        devuelveResultados = devuelveResultados + item;
-      } else {
-        devuelveResultados = devuelveResultados + '-' + item;
-      }
-
-      if (item % 2 == 1) {
-        contadorImpar++;
-      } else {
-        contadorPar++;
-      }
-
-      if (item < 9) {
-        secuencia1++;
-      }
-
-      if (item > 9 && item < 20) {
-        secuencia2++;
-      }
-
-      if (item > 19 && item < 30) {
-        secuencia3++;
-      }
-
-      if (item > 29) {
-        secuencia4++;
-      }
-
-    })
-    if (req.params.opcion == 1) {
-      // console.log(devuelveResultados);
-      asigna(devuelveResultados)
-
-    }
-    /******************TIPO**********/
-    if (req.params.opcion == 2) {
-      if (contadorPar > contadorImpar) {
-        // console.log("Par" + contadorPar);
-        asigna("Par" + contadorPar)
-      }
-
-      if (contadorPar < contadorImpar) {
-        // console.log("Impar" + contadorImpar);
-        asigna("Impar" + contadorImpar)
-      }
-
-      if (contadorPar == contadorImpar) {
-        // console.log("igual");
-        asigna("igual")
-      }
-    }
-    /***************FIN TIPO********/
-
-    /*************SECUENCIA********/
-
-    if (req.params.opcion == 3) {
-      // console.log(secuencia1 + " - " + secuencia2 + " - " + secuencia3 + " - " + secuencia4);
-      asigna(secuencia1 + " - " + secuencia2 + " - " + secuencia3 + " - " + secuencia4)
-    }
-
-    /**********FIN SECUENCIA*******/
-    contadorImpar, contadorImpra = 0;
-
-  });
-
-  var ver = [];
-  async function asigna(data) {
-    ver.push(data)
-  }
-
-
-  setTimeout(() => {
-    res.send(ver);
-  }, 100)
+    setTimeout(() => {
+        res.send(ver);
+    }, 100)
 
 
 
